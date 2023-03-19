7964bba399b42fc55d7d84300f415562aa51c336
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-03-19 00:22:29 -0400

    Create

---
+++ bash/install-tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install common languages
+sudo apt install python3 -y
+
+# Install Common Utilities
+sudo apt install nano net-tools zip -y
+
+# Install Common Fetchers
+sudo apt install curl git wget -y
+
+# Install Common Compilers
+sudo apt install g++ gcc make -y
