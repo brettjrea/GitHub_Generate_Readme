3dca8761a8add988bb5fceb4a56d79b3399038f4
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-03-19 01:43:54 -0400

    Create

---
+++ bash/configure-pm2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the directories for Strapi and Gatsby
+strapi_dir=~/my-backend
+gatsby_dir=~/my-frontend
+
+# Define the contents of the ecosystem.config.js file
+ecosystem_config="module.exports = {
+  apps: [
+    {
+      name: 'strapi',
+      script: 'cd ${strapi_dir} && yarn develop',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+    {
+      name: 'gatsby',
+      script: 'cd ${gatsby_dir} && yarn develop',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+};"
+
+# Remove existing ecosystem.config.js files if they exist
+rm -f ~/ecosystem.config.js
+rm -f ${strapi_dir}/ecosystem.config.js
+rm -f ${gatsby_dir}/ecosystem.config.js
+
+# Create the ecosystem.config.js file in the home directory
+echo "$ecosystem_config" > ~/ecosystem.config.js
+
+# Create the ecosystem.config.js files in the Strapi and Gatsby directories
+echo "$ecosystem_config" > ${strapi_dir}/ecosystem.config.js
+echo "$ecosystem_config" > ${gatsby_dir}/ecosystem.config.js
